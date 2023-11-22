87082ccdbd77a7534e6b085899653e92e01519f5
2023temitope <topbeemak@yahoo.com>
2023-11-22 10:24:04 +0000

    SAVING MY WORK

---
+++ WEB/javascript/loopsex.js
@@ -0,0 +1,57 @@
+for (let a=1; a<=10;a++);{
+    console.log ("hello");
+}
+
+
+for (let a =1; a <=100;a++){
+    if (a % 3=== 0 && a % 5===0){
+console.log("fizzbuzz");
+    }else if (a % 3===0) {
+        console.log("fizz");
+    }else if (a % 5===0) {
+        console.log ("buzz");
+    }else {
+        console.log (a="fizzbuzz")
+    } 
+ 
+
+let char=['1','2','3','4','5','6','7','8','9'];
+console.log(char[0]);
+console.log(char[1]);
+console.log(char[2]);
+console.log(char[3]);
+console.log(char[4]);
+console.log(char[5]);
+console.log(char[6]);
+console.log(char[7]);
+console.log(char[8]);
+console.log(char[9]);
+
+
+for (let a =0;a <=char.length; a++){
+    console.log(char[a]);
+}
+
+for (let a=char.length;a>=0; a--){
+    
+
+}
+
+   
+}
+for (let a =0;a <=char.length; a++){
+    console.log(char[a]);
+}
+
+for (let a=char.length;a>=0; a--){
+    
+    console.log(char[a]);
+
+
+}
+<script
+    src="index1.js">
+</script>
+     
+       
+     
